e95eab81f1fd8d97e942039ded9fb017433c039f
lukas-derksen <dev@lukasderksen.nl>
2025-06-05 21:45:25 +0200

    Moved vuetify settings to config file

--- popup/src/main.js
+++ popup/src/main.js
@@ -1,16 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
+import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
 
 loadFonts()
 
-const vuetify = createVuetify({
-  components,
-  directives
-});
 
 createApp(App)
   .use(vuetify)
